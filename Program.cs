69d31fc7b86684e594bd01ba57dd8285b3d0e89a
nicolas <Kiko2008.emilio@gmail.com>
2025-10-03 19:35:48 -0300

    finalizar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("--- MILHAS PARA KM ---");
+Console.Write("Informe o Valor em Milhas: ");
+
+string entrada = Console.ReadLine()!;
+double milhas = Convert.ToDouble(entrada);
+
+double resultado = milhas * 1.609;
+
+Console.WriteLine($"{milhas} milhas em km ----> {resultado}");
